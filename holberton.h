33220c70b5d058834f08b212bb37528f55e8f822
dcarrill-code <3346@holbertonschool.com>
2021-08-21 17:10:21 -0500

    Updating holberton.h

--- holberton.h
+++ holberton.h
@@ -1,6 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#define EOF             (-1)
+#define ERRCHAR         ( 0)
+
+#define INIT_SRC_POS    (-2)
+
 #include <signal.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,6 +16,14 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+struct source_s
+{
+    char *buffer;       /* the input text */
+    long bufsize;       /* size of the input text */
+    long  curpos;       /* absolute char position in source */
+};
+
+
 typedef struct list
 {
 	char *var;
@@ -20,5 +33,9 @@ typedef struct list
 void print_prompt1(void);
 void print_prompt2(void);
 char *read_cmd(void);
+char next_char(struct source_s *src);
+void unget_char(struct source_s *src);
+char peek_char(struct source_s *src);
+void skip_white_spaces(struct source_s *src);
 
 #endif
