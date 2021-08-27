df70c3235601ab7100b226a1a3693890c0633b6d
Yessica Bertel <ybertel0307@gmail.com>
2021-08-27 13:28:42 -0700

    done

---
+++ _strdup.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+char *_strdup(char *str)
+{
+	char *duplicate_str;
+	int i, len = 0;
+
+	if (str == NULL) /* validate str input */
+		return (NULL);
+
+	while (*(str + len))
+		len++;
+	len++; /* add null terminator to length */
+
+	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+	if (duplicate_str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
+	}
+
+	return (duplicate_str);
+}
--- prompt.c
+++ prompt.c
@@ -28,25 +28,25 @@ int built_in(char **token, list_t *env, int num, char **command)
 {
 	int i = 0;
 
-	if (strcmp(token[0], "exit") == 0)
+	if (_strcmp(token[0], "exit") == 0)
 		i = __exit(token, env, num, command);
 
-	else if (strcmp(token[0], "env") == 0)
+	else if (_strcmp(token[0], "env") == 0)
 	{
 		_env(token, env);
 		i = 1;
 	}
 
-	else if (strcmp(token[0], "cd") == 0)
+	else if (_strcmp(token[0], "cd") == 0)
 		i = _cd(token, env, num);
 
-	else if (strcmp(token[0], "setenv") == 0)
+	else if (_strcmp(token[0], "setenv") == 0)
 	{
 		_setenv(&env, token);
 		i = 1;
 	}
 
-	else if (strcmp(token[0], "unsetenv") == 0)
+	else if (_strcmp(token[0], "unsetenv") == 0)
 	{
 		_unsetenv(&env, token);
 		i = 1;
