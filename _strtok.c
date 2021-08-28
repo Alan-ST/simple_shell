332460e5f6d007be6f478e6e2582269479c03ee1
yessbertel <ybertel0307@gmail.com>
2021-08-27 23:06:40 -0500

    Update _strtok.c

--- _strtok.c
+++ _strtok.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
-
-
-
+/**
+ * t_strlen - returns token's string length for mallocing
+ * @str: a token
+ * @pos: index position in user's command typed into shell
+ * @delm: delimeter (e.g. " ");
+ * Return: token length
+ */
 int t_strlen(char *str, int pos, char delm)
 {
 	int len = 0;
