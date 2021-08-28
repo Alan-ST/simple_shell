be216a7a7af457a46076252cc24e966661ff19cf
yessbertel <ybertel0307@gmail.com>
2021-08-27 23:08:30 -0500

    Update error.c

--- error.c
+++ error.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
-
-
-
-
+/**
+ * not_found - write error ("sh: 1: lss: not found")
+ * @str: user's typed command
+ * @c_n: nth user's typed command
+ * @env: bring in environmental variables linked list to write shell name
+ */
 void not_found(char *str, int c_n, list_t *env)
 {
 	int count = 0;
@@ -29,7 +31,12 @@ void not_found(char *str, int c_n, list_t *env)
 	write(STDOUT_FILENO, "not found\n", 10);
 
 }
-
+/**
+ * cant_cd_to - write error ("sh: 2: cd: can't cd to xyz")
+ * @str: user's typed argument after the cmd cd
+ * @c_n: nth user's typed command
+ * @env: bring in environmental variables linked list to write shell name
+ */
 void cant_cd_to(char *str, int c_n, list_t *env)
 {
 	int count = 0;
